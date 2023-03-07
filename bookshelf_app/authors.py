00bcc4be93ed764d3f7ee29581acbdd2f69f46b1
piechajakub@wp.pl <piechajakub@wp.pl>
2023-03-07 11:51:49 +0100

    add post methods create authors

--- bookshelf_app/authors.py
+++ bookshelf_app/authors.py
@@ -1,29 +1,40 @@
-from flask import jsonify
-from bookshelf_app import app
-from
+from flask import jsonify, request
+from webargs.flaskparser import use_args
+from bookshelf_app import app, db
+from bookshelf_app.models import Author, AuthorSchema, author_schema
 
 
 @app.route('/api/v1/authors', methods=["GET"])
 def get_authors():
+    authors = Author.query.all()
+    author_schema = AuthorSchema(many=True)
     return jsonify({
         'success': True,
-        'data': 'Get all authors'
+        'data': author_schema.dump(authors),
+        'number of records': len(authors)
     })
 
 
 @app.route('/api/v1/authors/<int:author_id>', methods=["GET"])
 def get_author(author_id):
+    author = Author.query.get_or_404(author_id, description=f'Author with id {author_id} not found')
     return jsonify({
         'success': True,
-        'data': f'Get author id {author_id}'
+        'data': author_schema.dump(author)
     })
 
 
 @app.route('/api/v1/authors', methods=["POST"])
-def create_author():
+@use_args(author_schema)
+def create_author(args: dict):
+    author = Author(**args)
+
+    db.session.add(author)
+    db.session.commit()
+
     return jsonify({
         'success': True,
-        'data': 'Create new author'
+        'data': author_schema.dump(author)
     }), 201
 
 
--- bookshelf_app/models.py
+++ bookshelf_app/models.py
@@ -1,5 +1,8 @@
-from bookshelf_app import db
 
+
+from bookshelf_app import db
+from datetime import datetime
+from marshmallow import Schema, fields, validate, validates, ValidationError
 class Author(db.Model):
     __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +12,19 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'{self.__class__.__name__}: {self.first_name} {self.last_name}'
+
+
+class AuthorSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    first_name = fields.String(required=True, validate=validate.Length(max=50))
+    last_name = fields.String(required=True, validate=validate.Length(max=50))
+    birth_date = fields.Date('%d-%m-%Y', required=True)
+
+    @validates('birth_date')
+    def validate_birth_date(self, value):
+        if value > datetime.now().date():
+            raise ValidationError(f'Birth date must be lower than {datetime.now().date()}')
+
+
+
+author_schema = AuthorSchema()
