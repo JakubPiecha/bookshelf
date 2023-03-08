efc070da252a040da53432ecb3e4599dca608fb4
piechajakub@wp.pl <piechajakub@wp.pl>
2023-03-08 09:12:31 +0100

    add check fields function

--- bookshelf_app/models.py
+++ bookshelf_app/models.py
@@ -1,8 +1,8 @@
-
-
 from bookshelf_app import db
 from datetime import datetime
 from marshmallow import Schema, fields, validate, validates, ValidationError
+
+
 class Author(db.Model):
     __tablename__ = 'authors'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,6 +12,13 @@ class Author(db.Model):
 
     def __repr__(self):
         return f'{self.__class__.__name__}: {self.first_name} {self.last_name}'
+
+    @staticmethod
+    def get_schema_args(fields: str) -> dict:
+        schema_args = {'many': True}
+        if fields:
+            schema_args['only'] = [field for field in fields.split(',') if field in Author.__table__.columns]
+        return schema_args
 
 
 class AuthorSchema(Schema):
@@ -24,7 +31,6 @@ class AuthorSchema(Schema):
     def validate_birth_date(self, value):
         if value > datetime.now().date():
             raise ValidationError(f'Birth date must be lower than {datetime.now().date()}')
-
 
 
 author_schema = AuthorSchema()
